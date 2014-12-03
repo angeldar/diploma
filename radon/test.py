fd5ecb2387ff2d96c684cad98a7e23f4189f714f
Vasiliy Zemlyanov <angeldaren@gmail.com>
2014-12-04 01:10:43 +0300

    Add example of Holstead metrics usage.

--- radon/test.py
+++ radon/test.py
@@ -1,4 +1,7 @@
 from radon.cli_init import cc, raw, mi, log
+from radon.metrics import  h_visit
+
+from radon.tools import _open
 
 PATH = ['./tests']
 
@@ -21,4 +24,10 @@ for func in harvester._to_dicts()['tests\\test_file.py']:
 print(raw_harvester.as_json())
 
 print(mi_harvester.as_json())
+
+# Holstead metrics
+with _open('tests/test_file.py') as fobj:
+    holstead = h_visit(fobj.read())
+    print(holstead)
+
 print('End')
