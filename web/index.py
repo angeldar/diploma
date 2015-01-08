ccaa1be8c3e2cf254a92095378e5571819cd54e3
Vasiliy Zemlyanov <angeldaren@gmail.com>
2015-01-08 15:32:35 +0300

    Testhing ajax and line charts plotting.

--- web/index.py
+++ web/index.py
@@ -1,7 +1,30 @@
-from bottle import route, run, template, static_file
+import bottle
+from bottle import Bottle, request, response, route, run, template, static_file
+
+app = Bottle()
+
+@route('/img/<filename:re:.*\.png>')
+def send_image(filename):
+    return static_file(filename, root='static/img', mimetype='image/png')
+
+@route('/css/<filename:re:.*\.css>')
+def send_css(filename):
+    return static_file(filename, root='static/css', mimetype='text/css')
+
+@route('/js/<filename:re:.*\.js>')
+def send_js(filename):
+    return static_file(filename, root='static/js', mimetype='text/javascript')
+
+
+@app.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+@route('/ajax', method='GET')
+def get_ajax():
+    return {"value" : "string"}
 
 @route('/')
-@route('/hello')
 def index():
     return static_file('index.html', root='views')
     # return "Hello World!"
